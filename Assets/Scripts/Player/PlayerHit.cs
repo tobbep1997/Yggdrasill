0005763820552e5690101fd3ec47bb6eae10d5cd
tobbep1997 <tobbe.pogen@live.se>
2017-03-05 21:23:05 +0100

    message

--- Assets/Scripts/Player/PlayerHit.cs
+++ Assets/Scripts/Player/PlayerHit.cs
@@ -9,14 +9,50 @@ public class PlayerHit : MonoBehaviour {
     [SerializeField]
     Rigidbody2D rb2D;
 
-    bool dead = false;
+    [SerializeField]
+    PlayerHit other;
+
+    public bool dead = false;
+
+
+
+
+    float timer;
+    float time = 2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Weapond" && !dead)
         {
-            rb2D.AddForce(Vector2.up * 256);
-            col.enabled = false;
+            GetComponent<PlayerBehavior>().enableControlls = false;
+            dead = true;
+            Hit();
+
         }
     }
+
+    private void Update()
+    {
+        if (dead && !other.dead)
+        {
+            timer += Time.deltaTime;
+            if (timer > time)
+            {
+                
+                other.dead = true;
+                
+                other.Hit();
+            }
+            
+        }
+    }
+
+    public void Hit()
+    {
+        if (!dead)
+            return;
+        GetComponent<PlayerBehavior>().enableControlls = false;
+        rb2D.AddForce(Vector2.up * 256);
+        col.enabled = false;
+    } 
 }
